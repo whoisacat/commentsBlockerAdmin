dfda065636c51ad61b81c1b019f594f602de2e7d
whoisacat <whoisacat@gmail.com>
2022-11-23 15:38:07 +0500

    Проверка замены пути в скрипте запуска докер-компос

--- docker-compose/script.sh
+++ docker-compose/script.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+COUNT=$(cat ./docker-compose.yml | grep -c "Путь_до_папки")
+if [ $COUNT -gt 0 ]; then
+    echo "Замените Путь_до_папки в docker-compose.yml на ваш путь"
+    exit 1
+fi
 docker-compose -f ./docker-compose.yml up -d
