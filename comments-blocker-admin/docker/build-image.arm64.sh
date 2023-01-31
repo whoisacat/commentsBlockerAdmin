c52e017ccb6c23d77b1340005714224b6153a676
whoisacat <whoisacat@gmail.com>
2023-01-31 21:13:28 +0500

    added prometheus, micrometer and grafana

--- comments-blocker-admin/docker/build-image.arm64.sh
+++ comments-blocker-admin/docker/build-image.arm64.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ..
-docker build -f docker/Dockerfile -t whoisacat/ura_comments_blocker_admin:kafka_test.arm64 .
-docker push whoisacat/ura_comments_blocker_admin:kafka_test.arm64
+docker build -f docker/Dockerfile -t whoisacat/ura_comments_blocker_admin:prometheus_test.arm64 .
+docker push whoisacat/ura_comments_blocker_admin:prometheus_test.arm64
